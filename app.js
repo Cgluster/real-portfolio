ac9920ec228ae17c3669512875e7de11e9a0d38d
unknown <luster.clifford@yahoo.com>
2022-10-13 16:23:41 -0700

    updates again

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 // Button to go back to top of page
-// Button to change light to dark
+const showOnPx = 100;
+const backToTopButton = document.querySelector('.back-to-top');
+
+const scrollContainer = () => {
+    return document.documentElement || document.body;
+};
+
+document.addEventListener('scroll', () => {
+    if (scrollContainer().scrollTop > showOnPx) {
+        backToTopButton.classList.remove('hidden');
+    } else {
+        backToTopButton.classList.add('hidden');
+    }
+});
+
+const goToTop = () => {
+    document.body.scrollIntoView();
+}
+
+backToTopButton.addEventListener('click', goToTop);
